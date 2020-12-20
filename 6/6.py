fb2c9de3a4f93805cc45e1f35e403c23602de09c
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-12-20 15:52:00 +0200

    add 6.1

--- 6/6.py
+++ 6/6.py
@@ -1,15 +1,20 @@
 import os
+import re
 
 
 def get_data(file_name='data.txt'):
     data_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_name)
 
     with open(data_file) as f:
-        return f.read().strip().split('\n')
+        data = f.read().strip()
+
+    return re.split(r'\n\s*\n', data)
 
 
 def main():
     data = get_data()
+    sum_all = sum([len(set(d.replace('\n', ''))) for d in data])
+    print(sum_all)
 
 
 if __name__ == '__main__':
